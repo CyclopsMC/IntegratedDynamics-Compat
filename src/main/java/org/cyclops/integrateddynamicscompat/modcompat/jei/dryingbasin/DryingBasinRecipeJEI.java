e981d05f63946c5f0bccc32b255d09fa983a7d96
Ruben Taelman <rubensworks@gmail.com>
2017-07-30 12:45:51 +0200

    Upmerge 1.10 ID changes

--- src/main/java/org/cyclops/integrateddynamicscompat/modcompat/jei/dryingbasin/DryingBasinRecipeJEI.java
+++ src/main/java/org/cyclops/integrateddynamicscompat/modcompat/jei/dryingbasin/DryingBasinRecipeJEI.java
@@ -1,18 +1,16 @@
 package org.cyclops.integrateddynamicscompat.modcompat.jei.dryingbasin;
 
-import com.google.common.base.Function;
-import com.google.common.collect.Lists;
 import lombok.Data;
 import mezz.jei.api.ingredients.IIngredients;
-import mezz.jei.api.recipe.BlankRecipeWrapper;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidStack;
+import org.cyclops.cyclopscore.modcompat.jei.RecipeRegistryJeiRecipeWrapper;
 import org.cyclops.cyclopscore.recipe.custom.api.IRecipe;
+import org.cyclops.cyclopscore.recipe.custom.api.IRecipeRegistry;
 import org.cyclops.cyclopscore.recipe.custom.component.DurationRecipeProperties;
 import org.cyclops.cyclopscore.recipe.custom.component.ItemAndFluidStackRecipeComponent;
 import org.cyclops.integrateddynamics.block.BlockDryingBasin;
 
-import javax.annotation.Nullable;
 import java.util.List;
 
 /**
@@ -20,47 +18,48 @@ import java.util.List;
  * @author rubensworks
  */
 @Data
-public class DryingBasinRecipeJEI extends BlankRecipeWrapper {
+public class DryingBasinRecipeJEI extends RecipeRegistryJeiRecipeWrapper<BlockDryingBasin, ItemAndFluidStackRecipeComponent, ItemAndFluidStackRecipeComponent, DurationRecipeProperties, DryingBasinRecipeJEI> {
 
-    private final IRecipe<ItemAndFluidStackRecipeComponent, ItemAndFluidStackRecipeComponent, DurationRecipeProperties> recipe;
     private final List<ItemStack> inputItem;
     private final FluidStack inputFluid;
     private final List<ItemStack> outputItem;
     private final FluidStack outputFluid;
 
     public DryingBasinRecipeJEI(IRecipe<ItemAndFluidStackRecipeComponent, ItemAndFluidStackRecipeComponent, DurationRecipeProperties> recipe) {
-        this.recipe = recipe;
+        super(recipe);
         this.inputItem = recipe.getInput().getItemStacks();
         this.inputFluid = recipe.getInput().getFluidStack();
         this.outputItem = recipe.getOutput().getItemStacks();
         this.outputFluid = recipe.getOutput().getFluidStack();
     }
 
+    protected DryingBasinRecipeJEI() {
+        super(null);
+        this.inputItem = null;
+        this.inputFluid = null;
+        this.outputItem = null;
+        this.outputFluid = null;
+    }
+
+    @Override
+    protected IRecipeRegistry<BlockDryingBasin, ItemAndFluidStackRecipeComponent, ItemAndFluidStackRecipeComponent, DurationRecipeProperties> getRecipeRegistry() {
+        return BlockDryingBasin.getInstance().getRecipeRegistry();
+    }
+
+    @Override
+    protected DryingBasinRecipeJEI newInstance(IRecipe<ItemAndFluidStackRecipeComponent, ItemAndFluidStackRecipeComponent, DurationRecipeProperties> input) {
+        return new DryingBasinRecipeJEI(input);
+    }
+
     @Override
     public void getIngredients(IIngredients ingredients) {
-        ingredients.setInputs(ItemStack.class, inputItem);
-        ingredients.setOutputs(ItemStack.class, outputItem);
-        ingredients.setInput(FluidStack.class, inputFluid);
-        ingredients.setOutput(FluidStack.class, outputFluid);
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        return object instanceof DryingBasinRecipeJEI && ((DryingBasinRecipeJEI) object).recipe.equals(this.recipe);
-    }
-
-    @Override
-    public int hashCode() {
-        return 2 | this.recipe.hashCode();
+        ingredients.setInputs(ItemStack.class, getInputItem());
+        ingredients.setOutputs(ItemStack.class, getOutputItem());
+        ingredients.setInput(FluidStack.class, getInputFluid());
+        ingredients.setOutput(FluidStack.class, getOutputFluid());
     }
 
     public static List<DryingBasinRecipeJEI> getAllRecipes() {
-        return Lists.transform(BlockDryingBasin.getInstance().getRecipeRegistry().allRecipes(), new Function<IRecipe<ItemAndFluidStackRecipeComponent, ItemAndFluidStackRecipeComponent, DurationRecipeProperties>, DryingBasinRecipeJEI>() {
-            @Nullable
-            @Override
-            public DryingBasinRecipeJEI apply(IRecipe<ItemAndFluidStackRecipeComponent, ItemAndFluidStackRecipeComponent, DurationRecipeProperties> input) {
-                return new DryingBasinRecipeJEI(input);
-            }
-        });
+        return new DryingBasinRecipeJEI().createAllRecipes();
     }
 }
