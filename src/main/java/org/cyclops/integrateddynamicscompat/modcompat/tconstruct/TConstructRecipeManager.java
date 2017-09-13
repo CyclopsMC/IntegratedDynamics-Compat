f2dbac63ed3400fc2e2fe31f3dc754064cde0aa3
Ruben Taelman <rubensworks@gmail.com>
2017-09-13 17:16:15 +0200

    Restore TCon compat

--- src/main/java/org/cyclops/integrateddynamicscompat/modcompat/tconstruct/TConstructRecipeManager.java
+++ src/main/java/org/cyclops/integrateddynamicscompat/modcompat/tconstruct/TConstructRecipeManager.java
@@ -3,7 +3,7 @@ package org.cyclops.integrateddynamicscompat.modcompat.tconstruct;
 import net.minecraft.item.ItemStack;
 import org.cyclops.cyclopscore.recipe.custom.api.IRecipe;
 import org.cyclops.cyclopscore.recipe.custom.component.DurationRecipeProperties;
-import org.cyclops.cyclopscore.recipe.custom.component.ItemAndFluidStackRecipeComponent;
+import org.cyclops.cyclopscore.recipe.custom.component.IngredientAndFluidStackRecipeComponent;
 import org.cyclops.integrateddynamics.block.BlockCrystalizedChorusBlockConfig;
 import org.cyclops.integrateddynamics.block.BlockCrystalizedMenrilBlockConfig;
 import org.cyclops.integrateddynamics.block.BlockDryingBasin;
@@ -15,19 +15,19 @@ import slimeknights.tconstruct.library.TinkerRegistry;
 public class TConstructRecipeManager {
 
     public static void register() {
-        IRecipe<ItemAndFluidStackRecipeComponent, ItemAndFluidStackRecipeComponent, DurationRecipeProperties> recipeMenril = BlockDryingBasin.getInstance().getRecipeRegistry().findRecipeByOutput(
-                new ItemAndFluidStackRecipeComponent(
+        IRecipe<IngredientAndFluidStackRecipeComponent, IngredientAndFluidStackRecipeComponent, DurationRecipeProperties> recipeMenril = BlockDryingBasin.getInstance().getRecipeRegistry().findRecipeByOutput(
+                new IngredientAndFluidStackRecipeComponent(
                         new ItemStack(BlockCrystalizedMenrilBlockConfig._instance.getBlockInstance()), null));
         if (recipeMenril != null) {
-            TinkerRegistry.registerBasinCasting(recipeMenril.getOutput().getItemStack(), ItemStack.EMPTY,
+            TinkerRegistry.registerBasinCasting(recipeMenril.getOutput().getFirstItemStack(), ItemStack.EMPTY,
                     recipeMenril.getInput().getFluidStack().getFluid(), recipeMenril.getInput().getFluidStack().amount);
         }
 
-        IRecipe<ItemAndFluidStackRecipeComponent, ItemAndFluidStackRecipeComponent, DurationRecipeProperties> recipeChorus = BlockDryingBasin.getInstance().getRecipeRegistry().findRecipeByOutput(
-                new ItemAndFluidStackRecipeComponent(
+        IRecipe<IngredientAndFluidStackRecipeComponent, IngredientAndFluidStackRecipeComponent, DurationRecipeProperties> recipeChorus = BlockDryingBasin.getInstance().getRecipeRegistry().findRecipeByOutput(
+                new IngredientAndFluidStackRecipeComponent(
                         new ItemStack(BlockCrystalizedChorusBlockConfig._instance.getBlockInstance()), null));
         if (recipeChorus != null) {
-            TinkerRegistry.registerBasinCasting(recipeChorus.getOutput().getItemStack(), ItemStack.EMPTY,
+            TinkerRegistry.registerBasinCasting(recipeChorus.getOutput().getFirstItemStack(), ItemStack.EMPTY,
                     recipeChorus.getInput().getFluidStack().getFluid(), recipeChorus.getInput().getFluidStack().amount);
         }
     }
