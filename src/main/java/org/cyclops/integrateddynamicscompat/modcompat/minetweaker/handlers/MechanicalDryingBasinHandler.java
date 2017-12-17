44e831fb92220630335c4cab36e658e0566d8c28
Ruben Taelman <rubensworks@gmail.com>
2017-12-17 14:47:37 +0100

    Add crafttweaker integration for mechanical machines

---
+++ src/main/java/org/cyclops/integrateddynamicscompat/modcompat/minetweaker/handlers/MechanicalDryingBasinHandler.java
@@ -0,0 +1,55 @@
+package org.cyclops.integrateddynamicscompat.modcompat.minetweaker.handlers;
+
+import crafttweaker.annotations.ZenRegister;
+import crafttweaker.api.item.IItemStack;
+import crafttweaker.api.liquid.ILiquidStack;
+import org.cyclops.cyclopscore.modcompat.crafttweaker.handlers.RecipeRegistryHandler;
+import org.cyclops.cyclopscore.recipe.custom.Recipe;
+import org.cyclops.cyclopscore.recipe.custom.component.DurationRecipeProperties;
+import org.cyclops.cyclopscore.recipe.custom.component.IngredientAndFluidStackRecipeComponent;
+import org.cyclops.integrateddynamics.block.BlockMechanicalDryingBasin;
+import stanhebben.zenscript.annotations.Optional;
+import stanhebben.zenscript.annotations.ZenClass;
+import stanhebben.zenscript.annotations.ZenMethod;
+
+@ZenClass("mods.integrateddynamics.MechanicalDryingBasin")
+@ZenRegister
+public class MechanicalDryingBasinHandler extends RecipeRegistryHandler<BlockMechanicalDryingBasin, IngredientAndFluidStackRecipeComponent, IngredientAndFluidStackRecipeComponent, DurationRecipeProperties> {
+
+    private static final MechanicalDryingBasinHandler INSTANCE = new MechanicalDryingBasinHandler();
+
+    @Override
+    protected BlockMechanicalDryingBasin getMachine() {
+        return BlockMechanicalDryingBasin.getInstance();
+    }
+
+    @Override
+    protected String getRegistryName() {
+        return "MechanicalDryingBasin";
+    }
+
+    @ZenMethod
+    public static void addRecipe(@Optional IItemStack inputStack, @Optional ILiquidStack inputFluid,
+                           @Optional IItemStack outputStack, @Optional ILiquidStack outputFluid, int duration) {
+        INSTANCE.add(new Recipe<>(
+                new IngredientAndFluidStackRecipeComponent(RecipeRegistryHandler.toStack(inputStack), RecipeRegistryHandler.toFluid(inputFluid)),
+                new IngredientAndFluidStackRecipeComponent(RecipeRegistryHandler.toStack(outputStack), RecipeRegistryHandler.toFluid(outputFluid)),
+                new DurationRecipeProperties(duration)));
+    }
+
+    @ZenMethod
+    public static void removeRecipe(@Optional IItemStack inputStack, @Optional ILiquidStack inputFluid,
+                              @Optional IItemStack outputStack, @Optional ILiquidStack outputFluid, int duration) {
+        INSTANCE.remove(new Recipe<>(
+                new IngredientAndFluidStackRecipeComponent(RecipeRegistryHandler.toStack(inputStack), RecipeRegistryHandler.toFluid(inputFluid)),
+                new IngredientAndFluidStackRecipeComponent(RecipeRegistryHandler.toStack(outputStack), RecipeRegistryHandler.toFluid(outputFluid)),
+                new DurationRecipeProperties(duration)));
+    }
+
+    @ZenMethod
+    public static void removeRecipesWithOutput(@Optional IItemStack outputStack, @Optional ILiquidStack outputFluid) {
+        INSTANCE.remove(
+                new IngredientAndFluidStackRecipeComponent(RecipeRegistryHandler.toStack(outputStack), RecipeRegistryHandler.toFluid(outputFluid))
+        );
+    }
+}
---
+++ src/main/java/org/cyclops/integrateddynamicscompat/modcompat/minetweaker/handlers/MechanicalSqueezerHandler.java
@@ -0,0 +1,138 @@
+package org.cyclops.integrateddynamicscompat.modcompat.minetweaker.handlers;
+
+import com.google.common.collect.Lists;
+import crafttweaker.annotations.ZenRegister;
+import crafttweaker.api.item.IItemStack;
+import crafttweaker.api.liquid.ILiquidStack;
+import net.minecraft.item.crafting.Ingredient;
+import org.cyclops.cyclopscore.modcompat.crafttweaker.handlers.RecipeRegistryHandler;
+import org.cyclops.cyclopscore.recipe.custom.Recipe;
+import org.cyclops.cyclopscore.recipe.custom.component.DummyPropertiesComponent;
+import org.cyclops.cyclopscore.recipe.custom.component.DurationRecipeProperties;
+import org.cyclops.cyclopscore.recipe.custom.component.IngredientRecipeComponent;
+import org.cyclops.cyclopscore.recipe.custom.component.IngredientsAndFluidStackRecipeComponent;
+import org.cyclops.integrateddynamics.block.BlockMechanicalSqueezer;
+import stanhebben.zenscript.annotations.Optional;
+import stanhebben.zenscript.annotations.ZenClass;
+import stanhebben.zenscript.annotations.ZenMethod;
+
+import java.util.List;
+
+@ZenClass("mods.integrateddynamics.MechanicalSqueezer")
+@ZenRegister
+public class MechanicalSqueezerHandler extends RecipeRegistryHandler<BlockMechanicalSqueezer, IngredientRecipeComponent, IngredientsAndFluidStackRecipeComponent, DurationRecipeProperties> {
+
+    private static final MechanicalSqueezerHandler INSTANCE = new MechanicalSqueezerHandler();
+
+    @Override
+    protected BlockMechanicalSqueezer getMachine() {
+        return BlockMechanicalSqueezer.getInstance();
+    }
+
+    @Override
+    protected String getRegistryName() {
+        return "MechanicalSqueezer";
+    }
+
+    @ZenMethod
+    public static void addRecipe(IItemStack inputStack,
+                           @Optional IItemStack outputStack, @Optional ILiquidStack outputFluid, int duration) {
+        addRecipe(inputStack, outputStack, 1.0F, null, 1.0F, null, 1.0F, outputFluid, duration);
+    }
+
+    @ZenMethod
+    public static void addRecipe(IItemStack inputStack,
+                                 @Optional IItemStack outputStack1, @Optional float outputStackChance1,
+                                 @Optional IItemStack outputStack2, @Optional float outputStackChance2,
+                                 @Optional IItemStack outputStack3, @Optional float outputStackChance3,
+                                 @Optional ILiquidStack outputFluid, int duration) {
+        List<IngredientRecipeComponent> outputComponents = Lists.newArrayList();
+        if (outputStack1 != null) {
+            IngredientRecipeComponent outputComponent1 = new IngredientRecipeComponent(Ingredient.fromStacks(RecipeRegistryHandler.toStack(outputStack1)));
+            outputComponent1.setChance(outputStackChance1);
+            outputComponents.add(outputComponent1);
+        }
+        if (outputStack2 != null) {
+            IngredientRecipeComponent outputComponent2 = new IngredientRecipeComponent(Ingredient.fromStacks(RecipeRegistryHandler.toStack(outputStack2)));
+            outputComponent2.setChance(outputStackChance2);
+            outputComponents.add(outputComponent2);
+        }
+        if (outputStack3 != null) {
+            IngredientRecipeComponent outputComponent3 = new IngredientRecipeComponent(Ingredient.fromStacks(RecipeRegistryHandler.toStack(outputStack3)));
+            outputComponent3.setChance(outputStackChance3);
+            outputComponents.add(outputComponent3);
+        }
+        
+        INSTANCE.add(new Recipe<>(
+                new IngredientRecipeComponent(RecipeRegistryHandler.toStack(inputStack)),
+                new IngredientsAndFluidStackRecipeComponent(outputComponents, RecipeRegistryHandler.toFluid(outputFluid)),
+                new DurationRecipeProperties(duration)));
+    }
+
+    @ZenMethod
+    public static void removeRecipe(IItemStack inputStack,
+                              @Optional IItemStack outputStack, @Optional ILiquidStack outputFluid, int duration) {
+        removeRecipe(inputStack, outputStack, 1.0F, null, 1.0F, null, 1.0F, outputFluid, duration);
+    }
+
+    @ZenMethod
+    public static void removeRecipe(IItemStack inputStack,
+                                    @Optional IItemStack outputStack1, @Optional float outputStackChance1,
+                                    @Optional IItemStack outputStack2, @Optional float outputStackChance2,
+                                    @Optional IItemStack outputStack3, @Optional float outputStackChance3,
+                                    @Optional ILiquidStack outputFluid, int duration) {
+        List<IngredientRecipeComponent> outputComponents = Lists.newArrayList();
+        if (outputStack1 != null) {
+            IngredientRecipeComponent outputComponent1 = new IngredientRecipeComponent(Ingredient.fromStacks(RecipeRegistryHandler.toStack(outputStack1)));
+            outputComponent1.setChance(outputStackChance1);
+            outputComponents.add(outputComponent1);
+        }
+        if (outputStack2 != null) {
+            IngredientRecipeComponent outputComponent2 = new IngredientRecipeComponent(Ingredient.fromStacks(RecipeRegistryHandler.toStack(outputStack2)));
+            outputComponent2.setChance(outputStackChance2);
+            outputComponents.add(outputComponent2);
+        }
+        if (outputStack3 != null) {
+            IngredientRecipeComponent outputComponent3 = new IngredientRecipeComponent(Ingredient.fromStacks(RecipeRegistryHandler.toStack(outputStack3)));
+            outputComponent3.setChance(outputStackChance3);
+            outputComponents.add(outputComponent3);
+        }
+
+        INSTANCE.remove(new Recipe<>(
+                new IngredientRecipeComponent(RecipeRegistryHandler.toStack(inputStack)),
+                new IngredientsAndFluidStackRecipeComponent(outputComponents, RecipeRegistryHandler.toFluid(outputFluid)),
+                new DurationRecipeProperties(duration)));
+    }
+
+    @ZenMethod
+    public static void removeRecipesWithOutput(@Optional IItemStack outputStack, @Optional ILiquidStack outputFluid) {
+        removeRecipesWithOutput(outputStack, 1.0F, null, 1.0F, null, 1.0F, outputFluid);
+    }
+
+    @ZenMethod
+    public static void removeRecipesWithOutput(@Optional IItemStack outputStack1, @Optional float outputStackChance1,
+                                               @Optional IItemStack outputStack2, @Optional float outputStackChance2,
+                                               @Optional IItemStack outputStack3, @Optional float outputStackChance3,
+                                               @Optional ILiquidStack outputFluid) {
+        List<IngredientRecipeComponent> outputComponents = Lists.newArrayList();
+        if (outputStack1 != null) {
+            IngredientRecipeComponent outputComponent1 = new IngredientRecipeComponent(Ingredient.fromStacks(RecipeRegistryHandler.toStack(outputStack1)));
+            outputComponent1.setChance(outputStackChance1);
+            outputComponents.add(outputComponent1);
+        }
+        if (outputStack2 != null) {
+            IngredientRecipeComponent outputComponent2 = new IngredientRecipeComponent(Ingredient.fromStacks(RecipeRegistryHandler.toStack(outputStack2)));
+            outputComponent2.setChance(outputStackChance2);
+            outputComponents.add(outputComponent2);
+        }
+        if (outputStack3 != null) {
+            IngredientRecipeComponent outputComponent3 = new IngredientRecipeComponent(Ingredient.fromStacks(RecipeRegistryHandler.toStack(outputStack3)));
+            outputComponent3.setChance(outputStackChance3);
+            outputComponents.add(outputComponent3);
+        }
+
+        INSTANCE.remove(
+                new IngredientsAndFluidStackRecipeComponent(outputComponents, RecipeRegistryHandler.toFluid(outputFluid))
+        );
+    }
+}
