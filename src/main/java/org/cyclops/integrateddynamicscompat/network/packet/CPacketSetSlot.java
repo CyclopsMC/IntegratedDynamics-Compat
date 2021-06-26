67f58214db7591535c3ba439b779ce8a234c8466
Ruben Taelman <rubensworks@gmail.com>
2021-06-26 07:51:25 +0200

    Add sanity checks in set slot packet, Closes #27

--- src/main/java/org/cyclops/integrateddynamicscompat/network/packet/CPacketSetSlot.java
+++ src/main/java/org/cyclops/integrateddynamicscompat/network/packet/CPacketSetSlot.java
@@ -2,12 +2,15 @@ package org.cyclops.integrateddynamicscompat.network.packet;
 
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.inventory.container.Slot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import org.cyclops.cyclopscore.inventory.slot.SlotExtended;
 import org.cyclops.cyclopscore.network.CodecField;
 import org.cyclops.cyclopscore.network.PacketCodec;
+import org.cyclops.integrateddynamics.inventory.container.ContainerLogicProgrammerBase;
 
 /**
  * @author rubensworks
@@ -44,8 +47,13 @@ public class CPacketSetSlot extends PacketCodec {
 
     @Override
     public void actionServer(World world, ServerPlayerEntity player) {
-        if(player.openContainer.windowId == windowId) {
-            player.openContainer.putStackInSlot(slot, itemStack.copy());
+        if(player.openContainer instanceof ContainerLogicProgrammerBase
+                && player.openContainer.windowId == windowId
+                && slot < player.openContainer.inventorySlots.size()) {
+            final Slot itemSlot = player.openContainer.getSlot(slot);
+            if (itemSlot instanceof SlotExtended && ((SlotExtended) itemSlot).isPhantom()) {
+                player.openContainer.putStackInSlot(slot, itemStack.copy());
+            }
         }
     }
 
